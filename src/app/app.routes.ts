085e72a186b264eedf2b9a74f62f6ba08dfce95c
v.rublev <chibak-chuvak@mail.ru>
2025-02-02 19:31:35 +0500

    Lesson 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent,
+        title: 'List page',
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Details page',
+    },
+    
+];
